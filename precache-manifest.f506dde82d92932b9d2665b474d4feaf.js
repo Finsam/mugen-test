540ba2750c1a8113cb24e46d611492574fb849ec
Finsam <finsam@stanford.edu>
2022-04-03 14:16:57 -0700

    Updates

---
+++ precache-manifest.f506dde82d92932b9d2665b474d4feaf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a644e3cc8a760c1080dc4356d9bc63f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "225285610b3c3f9102c7",
+    "url": "./static/css/2.0032627e.chunk.css"
+  },
+  {
+    "revision": "4564a419ae8c655b5b86",
+    "url": "./static/css/main.4ffb71ae.chunk.css"
+  },
+  {
+    "revision": "225285610b3c3f9102c7",
+    "url": "./static/js/2.e2b79412.chunk.js"
+  },
+  {
+    "revision": "4564a419ae8c655b5b86",
+    "url": "./static/js/main.bc217c0d.chunk.js"
+  },
+  {
+    "revision": "16a42cde83f893ac106e",
+    "url": "./static/js/runtime-main.7252612a.js"
+  },
+  {
+    "revision": "5f54dc842febb046a8801d67b0d941ae",
+    "url": "./static/media/Starsback.5f54dc84.jpg"
+  },
+  {
+    "revision": "92d96a5f2923bfc9c5399ff1900a8632",
+    "url": "./static/media/bck1.92d96a5f.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "./static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "./static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "./static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "./static/media/slick.f97e3bbf.svg"
+  }
+]);
